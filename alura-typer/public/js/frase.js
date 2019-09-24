4f1c8576bb47b12fa55184ca9e4a029100f804bb
Rodrigo Viana de Oliveira <roviol@gmail.com>
2019-09-24 20:26:25 -0300

    Implementação de um campo para o usuário escolher a frase.

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
     $("#spinner").toggle();
@@ -22,4 +23,28 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+    console.log("Id da minha frase: " + fraseId);
+    var dados = {id: fraseId};
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
